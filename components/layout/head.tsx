3e105696b9dfcc25df205847fcd55102a7a78b07
mac <mac.develops@icloud.com>
2021-07-02 20:21:31 +0100

    update: pwa functionality working

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -28,27 +28,29 @@ export default function Head({
 }: HeadProps) {
   return (
     <NextHead>
-      <meta charSet='UTF-8'></meta>
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1'></meta>
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='description' content={pageDescription} />
+      <meta property='og:title' content={`${pageTitle} | ${siteTitle}`} />
+      <meta property='og:description' content={pageDescription} />
+      <meta property='og:image' content={socialImage} />
+      <meta property='og:image:alt' content={socialImageText} />
+      <meta property='og:locale' content={siteLocale} />
+      <meta property='og:type' content={siteType} />
+      <meta property='og:url' content={siteUrl} />
+      <meta name='twitter:card' content={socialImage} />
+      <meta name='theme-color' content={themeColor} />
+      <link rel='canonical' href={siteUrl} />
+      <link rel='icon' href={siteIcon} type='image/svg+xml' />
+      <link rel='apple-touch-icon' href={touchIcon} />
+      <link
+        rel='manifest'
+        crossOrigin='use-credentials'
+        href='/manifest.json'
+      />
       <title>
         {pageTitle} | {siteTitle}
       </title>
-      <meta name='description' content={pageDescription}></meta>
-      <meta property='og:title' content={`${pageTitle} | ${siteTitle}`}></meta>
-      <meta property='og:description' content={pageDescription}></meta>
-      <meta property='og:image' content={socialImage}></meta>
-      <meta property='og:image:alt' content={socialImageText}></meta>
-      <meta property='og:locale' content={siteLocale}></meta>
-      <meta property='og:type' content={siteType}></meta>
-      <meta property='og:url' content={siteUrl}></meta>
-      <meta name='twitter:card' content={socialImage}></meta>
-      <link rel='canonical' href={siteUrl}></link>
-      <link rel='icon' href={siteIcon} type='image/svg+xml'></link>
-      <link rel='apple-touch-icon' href={touchIcon}></link>
-      <link rel='manifest' href='/manifest.json'></link>
-      <meta name='theme-color' content={themeColor}></meta>
     </NextHead>
   )
 }
